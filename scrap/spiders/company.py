781182a49853c7eb0d560a8cb6ec5bb95f1ddef2
mehdi <mahdishahidi010@hmail.com>
2022-06-07 19:34:59 +0430

    create: company spider

---
+++ scrap/spiders/company.py
@@ -0,0 +1,16 @@
+import scrapy
+from ..items import ScrapItem
+
+
+class EsgSpider(scrapy.Spider):
+    name = 'esg'
+    allowed_domains = ['www.refinitiv.com']
+    start_urls = ['https://www.refinitiv.com/bin/esg/esgsearchsuggestions']
+
+    def parse(self, response, **kwargs):
+        data = ScrapItem()
+        items = response.json()
+        for item in items:
+            data['companyName'] = item['companyName']
+            data['ricCode'] = item['ricCode']
+            yield data
