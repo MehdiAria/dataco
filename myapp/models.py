f4f8a38f5692996c26c428856926917f9e42be90
mehdi <mahdishahidi010@hmail.com>
2022-06-07 19:29:31 +0430

    Create: model and view init

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Company(models.Model):
+    companyName = models.CharField(max_length=255, null=False)
+    ricCode = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.companyName
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.views.generic import ListView
+
+
+class CompanyListView(ListView):
+    ...
