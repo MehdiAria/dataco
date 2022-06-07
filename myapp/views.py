26a590a6083ab7be2dced70aee7bc05e7f74815a
mehdi <mahdishahidi010@hmail.com>
2022-06-07 20:05:32 +0430

    Feat: simple view to show data

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.views.generic import ListView
 
+from myapp.models import Company
+
 
 class CompanyListView(ListView):
-    ...
+    model = Company
+    queryset = Company.objects.all()
+    context_object_name = 'items'
+
+    def get(self, request, *args, **kwargs):
+        company_list = Company.objects.all()
+        context = {'company_list': company_list}
+        return render(request, 'company_list.html', context=context)
