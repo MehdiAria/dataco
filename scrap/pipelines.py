8f4f8471fd656af72f5cb06025970fa9aadea529
mehdi <mahdishahidi010@hmail.com>
2022-06-09 15:00:38 +0430

    Add: Esg pipline and inheritance.

--- scrap/pipelines.py
+++ scrap/pipelines.py
@@ -16,9 +16,10 @@ from itemadapter import ItemAdapter
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import sqlite3
+from .items import EsgItem, ScrapItem
 
 
-class ScrapPipeline(object):
+class Pipeline(object):
     def __init__(self):
         """Constructor"""
         self.create_connection()
@@ -31,7 +32,36 @@ class ScrapPipeline(object):
 
     def create_table(self):
         """"Create a table in the database"""
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS myapp_company (companyName textl,ricCode text)""")
+        pass
+
+    def store_data(self, item):
+        """Store data in the database"""
+        pass
+
+    def process_item(self, item, spider):
+        """Process the item"""
+        # if isinstance(item, EsgItem):
+        #     print('ESG','$'*100000)
+        # if isinstance(item, ScrapItem):
+        #     print('ScrapItem','$'*100000)
+        try:
+            self.store_data(item)
+        except KeyError as e:
+            # raise e
+            pass
+        return item
+
+
+class ScrapPipeline(Pipeline):
+    def __init__(self):
+        super().__init__()
+
+    def create_connection(self):
+        super().create_connection()
+
+    def create_table(self):
+        """"Create a table in the database"""
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS myapp_company (companyName text,ricCode text)""")
 
     def store_data(self, item):
         """Store data in the database"""
@@ -40,6 +70,32 @@ class ScrapPipeline(object):
         self.cnt.commit()
 
     def process_item(self, item, spider):
-        """Process the item"""
-        self.store_data(item)
-        return item
+        return super().process_item(item, spider)
+
+
+class EsgPipeline(Pipeline):
+    def __init__(self):
+        super().__init__()
+
+    def create_connection(self):
+        super().create_connection()
+
+    def create_table(self):
+        """"Create a table in the database"""
+        self.cur.execute(
+            """CREATE TABLE IF NOT EXISTS myapp_esg (esg text,environment text,social text,governance text,rank text,
+            total text,ricCode text)""")
+        print('CREATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
+    def store_data(self, item):
+        """Store data in the database"""
+        self.cur.execute(
+            """INSERT INTO myapp_esg (esg,environment,social,governance,rank,total,ricCode)VALUES(?,?,?,?,?,?,?);""",
+            (item['esg'], item['environment'], item['social'], item['governance'], item['rank'], item['total'],
+             item['ricCode']))
+        print('INSERT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
+        self.cnt.commit()
+
+    def process_item(self, item, spider):
+        return super().process_item(item, spider)
