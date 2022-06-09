2a01d40eca1b27ea7ac1f6636b82fcbc61418ebe
mehdi <mahdishahidi010@hmail.com>
2022-06-09 14:59:51 +0430

    Add: ESG Model

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
 
-# Create your models here.
 class Company(models.Model):
     """Company model"""
-    companyName = models.CharField(max_length=255, null=False) # Company name
-    ricCode = models.CharField(max_length=255, null=False) # Code
+    companyName = models.CharField(max_length=255, null=False)  # Company name
+    ricCode = models.CharField(max_length=255, null=False)  # Code
 
     def __str__(self):
         """Returns a string representation of the model"""
         return self.companyName
+
+
+class Esg(models.Model):
+    """Company model"""
+    esg = models.CharField(max_length=5, null=False)
+    environment = models.CharField(max_length=5, null=False)
+    social = models.CharField(max_length=5, null=False)
+    governance = models.CharField(max_length=5, null=False)
+    rank = models.CharField(max_length=5, null=False)
+    total = models.CharField(max_length=5, null=False)
+    ricCode = models.CharField(max_length=5, null=False)
+
+    def __str__(self):
+        """Returns a string representation of the model"""
+        return self.ricCode
