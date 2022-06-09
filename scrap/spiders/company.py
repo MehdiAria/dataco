d300c16d3fddba7ba68f1150b8ca6743db28a5ac
mehdi <mahdishahidi010@hmail.com>
2022-06-09 14:59:14 +0430

    Feat: scrapy request to scrap esg info

--- scrap/spiders/company.py
+++ scrap/spiders/company.py
@@ -1,5 +1,10 @@
 import scrapy
-from ..items import ScrapItem
+from scrapy import FormRequest
+from scrapy.spiders import CrawlSpider
+from scrapy import Request
+from ..items import ScrapItem, EsgItem
+from urllib.parse import urljoin
+import json
 
 
 class EsgSpider(scrapy.Spider):
@@ -15,3 +20,27 @@ class EsgSpider(scrapy.Spider):
             data['companyName'] = item['companyName']
             data['ricCode'] = item['ricCode']
             yield data
+            yield Request(f"https://www.refinitiv.com/bin/esg/esgsearchresult?ricCode={item['ricCode']}",
+                          callback=self.parse2)
+        # print('+' * 1000)
+
+    def parse2(self, response):
+        data = EsgItem()
+        # print('-' * 1000)
+        a = json.loads(response.css('p::text').get())
+        esg = a['esgScore']['TR.TRESG']['score']
+        environment = a['esgScore']['TR.EnvironmentPillar']['score']
+        social = a['esgScore']['TR.SocialPillar']['score']
+        governance = a['esgScore']['TR.GovernancePillar']['score']
+        rank = a['industryComparison']['rank']
+        total = a['industryComparison']['totalIndustries']
+        code = response.url[58:]
+        data['esg'] = esg
+        data['environment'] = environment
+        data['social'] = social
+        data['governance'] = governance
+        data['rank'] = rank
+        data['total'] = total
+        data['ricCode'] = code
+        # print('-' * 1000)
+        yield data
