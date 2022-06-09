810d485ca25cc78d96b5c3ed7c8144d4dabcebda
mehdi <mahdishahidi010@hmail.com>
2022-06-09 17:35:28 +0430

    Fix: Sent slug correctly

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from myapp.views import CompanyListView, CompanyListAPIView
+from myapp.views import CompanyListView, CompanyListAPIView, EsgDetailsView, EsgDetailsAPIView
 
 urlpatterns = [
-    path('list/', CompanyListView.as_view(), name='company-list'), # myapp/views.py
-    path('api/corps/', CompanyListAPIView.as_view(), name='company-list-api'), # myapp/views.py
+    path('list/', CompanyListView.as_view(), name='company-list'),  # myapp/views.py
+    path('detail/<slug:code>', EsgDetailsView.as_view(), name='company-detail'),  # myapp/views.py
+    path('api/corps/', CompanyListAPIView.as_view(), name='company-list-api'),  # myapp/views.py
+    path('api/corps/esgscore/<slug:code>', EsgDetailsAPIView.as_view(), name='company-detail-api'),  # myapp/views.py
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,12 +5,12 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from rest_framework import permissions
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
-from myapp.models import Company
-from myapp.serializer import CompanySerializer
+from myapp.models import Company, Esg
+from myapp.serializer import CompanySerializer, EsgSerializer
 
 
 class CompanyListView(ListView):
@@ -37,7 +37,31 @@ class CompanyListAPIView(ListAPIView):
     pagination_class = CustomNumberPagination
     permission_classes = [IsAuthenticated]
 
-# TODO
-# class EsgDetailsView(DetailView):
-#     model = Esg
-#     template_name = 'esg_detail.html'
+
+from django.shortcuts import get_object_or_404
+
+
+class EsgDetailsView(DetailView):
+    model = Esg
+    template_name = 'esg_detail.html'
+    context_object_name = 'items'
+    slug_field = 'ricCode'
+    slug_url_kwarg = 'code'
+    # def setup(self, request, *args, **kwargs):
+    #     esg_instance = get_object_or_404(Esg, ricCode=kwargs['code'])
+    #     # print('A' * 50, esg_instance)
+    #     return super().setup(request, *args, **kwargs)
+    #
+    # def get(self, request, *args, **kwargs):
+    #     self.object = self.get_object()
+    #     context = self.get_context_data(object=self.object)
+    #     return self.render_to_response(context)
+    # slug_url_kwarg = 'ricCode'
+    # context_object_name = 'slug'
+
+
+class EsgDetailsAPIView(RetrieveAPIView):
+    serializer_class = EsgSerializer
+    queryset = Esg.objects.all()
+    lookup_field = 'ricCode'
+    lookup_url_kwarg = 'code'
